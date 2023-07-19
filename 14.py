0e8482d67493f9f6c3ea0584ac3cd93a4b2a89a4
Aliona Matweyenko <matveenkoalena2844@mail.ru>
2023-07-19 15:15:36 +0300

    Improve details

--- 14.py
+++ 14.py
@@ -10,16 +10,23 @@ list_data = input_data.split()
 chislo = 77
 
 if len(list_data) == 1:
-    cursor.execute('''INSERT INTO table_one(column_one) VALUES (?)''', (int(list_data[0]),))
-    conn.commit()
+    if list_data[0].isnumeric():
+        cursor.execute('''INSERT INTO table_one(column_one) VALUES (?)''', (int(list_data[0]),))
+        conn.commit()
+    else:
+        print('Параметр не число')
 elif len(list_data) == 2:
-    if type(list_data[1]) is int:
-        cursor.execute('''DELETE FROM table_one WHERE ID = 1''')
+    if list_data[1].isnumeric():
+        cursor.execute('''DELETE FROM table_one WHERE ID = (SELECT MIN(ID) FROM table_one)''')
         conn.commit()
+    else:
+        print('Второй параметр не число')
 elif len(list_data) == 3:
-    if type(list_data[2]) is int:
-        cursor.execute('''UPDATE table_one SET column_one = ? WHERE ID = 3''', (chislo,))
+    if list_data[2].isnumeric():
+        cursor.execute('''UPDATE table_one SET column_one = ? WHERE ID = (SELECT ID FROM table_one LIMIT 1 OFFSET 2)''', (chislo,))
         conn.commit()
+    else:
+        print('Третий параметр не число')
 
 cursor.execute('''SELECT * FROM table_one''')
 k = cursor.fetchall()
