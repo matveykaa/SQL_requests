786168dd5bebbbca23da014c72854b43df71b9d5
Aliona Matweyenko <matveenkoalena2844@mail.ru>
2023-07-24 11:44:30 +0300

    Edit function

--- 16_2.py
+++ 16_2.py
@@ -4,9 +4,16 @@
 
 import sqlite3
 
+def add_info(id:int, name: str):
+    cursor.execute('''SELECT count(*) FROM table16_2 WHERE ID=?''', (id,))
+    if cursor.fetchone()[0]:
+        print('\n- - ID exists - -')
+    else:
+        cursor.execute('''INSERT INTO table16_2(ID, name) VALUES (?, ?)''', (id, name))
+
+
 conn = sqlite3.connect('name16_2.db')
 cursor = conn.cursor()
-
 cursor.execute('''CREATE TABLE IF NOT EXISTS table16_2(ID INTEGER PRIMARY KEY, name TEXT)''')
 
 while True:
@@ -16,13 +23,9 @@ while True:
     inp_name = input_data.split()[1]
     if input_data.split()[0].isnumeric():
         inp_id = int(input_data.split()[0])
-        cursor.execute('''SELECT count(*) FROM table16_2 WHERE ID=?''', (inp_id,))
-        if cursor.fetchone()[0]:
-            print('\n- - ID exists - -')
-        else:
-            cursor.execute('''INSERT INTO table16_2(ID, name) VALUES (?, ?)''', (inp_id, inp_name))
+        add_info(inp_id, inp_name)
     else:
-        print('- - ID should be numeric - -')
+        print('\n- - ID should be numeric - -')
     cursor.execute('''SELECT * FROM table16_2''')
     k = cursor.fetchall()
     print(k)
