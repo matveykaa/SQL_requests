6935dab5e551873375d0b8b530d5a607d7910d07
Aliona Matweyenko <matveenkoalena2844@mail.ru>
2023-07-24 13:07:21 +0300

    Add 16_5 task

---
+++ 16_5.py
@@ -0,0 +1,17 @@
+# Создайте метод класса для работы с БД, который выводит все записи из таблицы.
+
+
+import sqlite3
+
+conn = sqlite3.connect('name16_5.db')
+cursor = conn.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS table16_5(ID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)''')
+
+input_data = input('Введите text - ')
+cursor.execute('''INSERT INTO table16_5(name) VALUES (?)''', (input_data,))
+conn.commit()
+
+cursor.execute('''SELECT * FROM table16_5''')
+k = cursor.fetchall()
+print(k)
